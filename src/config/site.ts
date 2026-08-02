fbf3e4026f53437aec2e7871c3857fc5be47f9cf
niklasnikvisualsmedia333 <info@nikvisuals.de>
2026-08-02 21:31:29 +0200

    feat: add LinkedIn aftermovie links to event recaps

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,10 +6,16 @@ export const SITE = {
   whatsapp: 'https://chat.whatsapp.com/Is1hh61VskMLHRgjqgJNUH',
   instagram: 'https://www.instagram.com/techmeetsproblems/',
   linkedin: 'https://www.linkedin.com/company/tech-meets-problems',
+  aftermovie: {
+    url: 'https://www.linkedin.com/posts/niklas-bruene_techmeetsproblems-openinnovation-siegen-ugcPost-7488305972478889984-6u6s/?utm_source=share&utm_medium=member_desktop&rcm=ACoAADx2fLkB0ZZvBko-ImmB1_SvR0E3SiwrNaw',
+    label: {
+      de: 'Aftermovie auf LinkedIn ansehen',
+      en: 'Watch the aftermovie on LinkedIn',
+    },
+  },
   imprint: 'https://nikvisuals.de/impressum',
   supporters: {
     startpunkt57: 'https://startpunkt57.de/',
     entrepreneurshipCenter: 'https://www.uni-siegen.de/startup/',
   },
 } as const;
-
